be70647a9439897340c4bdf0ba170f0f070aa66f
msd1729 <msd1729@gmail.com>
2015-03-11 20:10:49 -0700

    Parsed JSON api

---
+++ JSON_api/TrackCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackCell.swift
+//  JSON_api
+//
+//  Created by Sachin Dheeraj on 3/11/15.
+//  Copyright (c) 2015 Sachin. All rights reserved.
+//
+
+import UIKit
+class TrackCell: UITableViewCell {
+    @IBOutlet weak var playIcon: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+}
